75cd6224a4e504848c0ca2a05e872609303878dd
Nipa <akternipa941@gmail.com>
2023-03-20 20:06:25 +0600

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div >
-     
-    </div>
-  );
+  return <div>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>;
 }
 
 export default App;
